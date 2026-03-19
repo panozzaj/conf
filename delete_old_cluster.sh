9dd528e3fae30caec9575c1b8d5e1d400a906ded
Anthony Panozzo <panozzaj@gmail.com>
2026-03-18 21:42:13 -0400

    Add PostgreSQL maintenance scripts (cluster cleanup, extension updates)

---
+++ delete_old_cluster.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rm -rf '/opt/homebrew/var/postgresql@16'
---
+++ update_extensions.sql
@@ -0,0 +1,131 @@
+\connect apply_yourself_test8
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+ALTER EXTENSION "citext" UPDATE;
+\connect apply_yourself_test5
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+ALTER EXTENSION "citext" UPDATE;
+\connect apply_yourself_test2
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+ALTER EXTENSION "citext" UPDATE;
+\connect apply_yourself_test4
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+ALTER EXTENSION "citext" UPDATE;
+\connect apply_yourself_test6
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+ALTER EXTENSION "citext" UPDATE;
+\connect apply_yourself_test9
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+ALTER EXTENSION "citext" UPDATE;
+\connect apply_yourself_test11
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+ALTER EXTENSION "citext" UPDATE;
+\connect apply_yourself_test13
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+ALTER EXTENSION "citext" UPDATE;
+\connect apply_yourself_test12
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+ALTER EXTENSION "citext" UPDATE;
+\connect apply_yourself_test3
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+ALTER EXTENSION "citext" UPDATE;
+\connect apply_yourself_test7
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+ALTER EXTENSION "citext" UPDATE;
+\connect apply_yourself_test10
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+ALTER EXTENSION "citext" UPDATE;
+\connect apply_yourself_test14
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+ALTER EXTENSION "citext" UPDATE;
+\connect feedbin_development
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect feedbin_test
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect apply_yourself_development_temp_20250924150353_old
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+ALTER EXTENSION "citext" UPDATE;
+\connect apply_yourself_staging
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+ALTER EXTENSION "citext" UPDATE;
+\connect possip_development
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect apply_yourself_development
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+ALTER EXTENSION "citext" UPDATE;
+\connect apply_yourself_cleanup_test
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+ALTER EXTENSION "citext" UPDATE;
+\connect apply_yourself_development_backup_20251019_083604
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+ALTER EXTENSION "citext" UPDATE;
+\connect possip_test1
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect possip_test10
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect possip_test7
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect possip_test4
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect possip_test9
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect possip_test12
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect possip_test6
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect possip_test14
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect possip_test8
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect possip_test5
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect possip_test11
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect possip_test13
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect possip_test3
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect possip_test2
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect possip_test
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect apply_yourself_test
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+ALTER EXTENSION "citext" UPDATE;
+\connect scout_development
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect scout_test11
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect video_scanner_dev
+ALTER EXTENSION "pgcrypto" UPDATE;
+\connect video_scanner_test
+ALTER EXTENSION "pgcrypto" UPDATE;
+\connect scout_test12
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect scout_test14
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect scout_test13
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect scout_test16
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect scout_test15
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect scout_test
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect scout_test3
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect scout_test5
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect scout_test6
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect scout_test9
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect scout_test10
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect scout_test2
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect scout_test8
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect scout_test7
+ALTER EXTENSION "pg_stat_statements" UPDATE;
+\connect scout_test4
+ALTER EXTENSION "pg_stat_statements" UPDATE;
